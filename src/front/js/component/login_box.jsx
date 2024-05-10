bb36d1d6e5eb47a590d785ac3a785ac08409011e
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-05-10 22:36:40 +0000

    very basic login page

---
+++ src/front/js/component/login_box.jsx
@@ -0,0 +1,33 @@
+import React, { useContext, useState } from "react";
+import { Context } from "../store/appContext";
+
+const LoginBox = () => {
+	const { actions } = useContext(Context);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function login(event) {
+        event.preventDefault();
+        console.log(email, password)
+        actions.login(email, password);
+    }
+
+    return (
+        <div className="card col-10">
+            <h2>Login</h2>
+            <form onSubmit={login}>
+                <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} />
+                </div>
+                <div className="form-group mb-3">
+                    <label htmlFor="exampleInputPassword1">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+                </div>
+                <button type="submit" className="btn btn-primary w-100 mb-3">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default LoginBox;
