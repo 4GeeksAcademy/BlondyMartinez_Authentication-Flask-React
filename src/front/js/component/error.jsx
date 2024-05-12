1d145701761be80acf8f8e92d8116f0a8959c475
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-05-12 13:11:15 +0000

    now api errors are displayed as alerts

---
+++ src/front/js/component/error.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ErrorAlert = ({ error }) => {
+    return (
+        <div className="alert alert-danger" role="alert">
+            {error}
+        </div>
+    )
+}
+
+export default ErrorAlert;
