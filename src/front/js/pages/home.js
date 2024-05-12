5ec8397355147b26096a03e1abe50ba3d0195976
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-05-12 13:10:49 +0000

    now keeps track of login errors and added ability to logout

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,15 +1,16 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 import LoginForm from "../component/login_form.jsx";
+import ErrorAlert from "../component/error.jsx";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+	const { store } = useContext(Context);
 
 	return (
 		<div className="container mx-auto mt-3">
 			<div className="row">
+				{store.login_error && <ErrorAlert error={store.login_error} />}
 				<LoginForm />
 			</div>
 		</div>
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -2,6 +2,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
 			access_token: "",
+			login_error: "",
 		},
 		actions: {
 			login: (email, password) => {
@@ -13,9 +14,20 @@ const getState = ({ getStore, getActions, setStore }) => {
 				}
 
 				fetch(process.env.BACKEND_URL + "/login", config)
-				.then((response) => response.json())
-				.then((data) => setStore({ access_token: data.access_token }))
+				.then((response) => {
+					if (!response.ok) {
+						return response.json().then((error) => {
+							setStore({ login_error: error.Error })
+						  	throw new Error(error.Error);
+						});
+					} else return response.json(); 
+				})
+				.then((data) => setStore({ access_token: data.access_token, login_error: "" }))
 				.catch((error) => console.log(error))
+			},
+
+			logout: () => {
+				setStore({ access_token: "" })
 			}
 		}
 	};
